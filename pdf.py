1d8a476ed2de85cd8f6992397c7ce5ef86b7ee75
zimenkoilya <zimenkoilya2@outlook.com>
2023-07-23 14:02:29 +1000

    first

---
+++ pdf.py
@@ -0,0 +1,15 @@
+from PyPDF2 import PdfReader
+pdf_file = open('sample.pdf', 'rb')
+pdf_reader = PdfReader(pdf_file)
+num_pages = len(pdf_reader.pages)
+data = ''
+for page_number in range(num_pages):
+    page = pdf_reader.pages[page_number]
+    text = page.extract_text()
+    data += text
+with open('sample.txt', 'w', encoding='utf-8') as file:
+    file.write(data)
+
+file.close()
+pdf_file.close()
+
